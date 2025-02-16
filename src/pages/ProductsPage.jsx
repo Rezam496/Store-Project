054ce7401a1e6be425f1668cea8861ee186e6fe3
Reza Mohammadi <reza.mohammadi9727@gmail.com>
2025-02-16 16:20:13 +0330

    Creating a Card Component and Displaying Products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,10 +1,17 @@
+import Card from "../components/Card";
 import { useProducts } from "../context/ProducContext"
-
+import styles from './ProductsPage.module.css'
 function ProductsPage() {
   const products=useProducts();
-  console.log(products);
+
   return (
-    <div>ProductsPage</div>
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length&&<p>Loading... </p>}
+        {products.map((p)=>(<Card key={p.id} data={p}/>))}
+      </div>
+      <div>sidebar</div>
+    </div>
   )
 }
 
