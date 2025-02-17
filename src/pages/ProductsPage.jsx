8d7ee85f0605b8ed843c5a41f536528fa40fe295
Reza Mohammadi <reza.mohammadi9727@gmail.com>
2025-02-17 11:43:21 +0330

    Add search and categorization to the project

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,17 +1,47 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 import Card from "../components/Card";
-import { useProducts } from "../context/ProducContext"
-import styles from './ProductsPage.module.css'
+import Loader from "../components/Loader";
+import { useProducts } from "../context/ProducContext";
+import {filterProducts, getInitialQuery, searchProducts } from "../helper/helper";
+
+import styles from './ProductsPage.module.css';
+import SearchBox from "../components/SearchBox";
+import Sidebar from "../components/Sidebar";
+
 function ProductsPage() {
   const products=useProducts();
+  
+  const [displayed,setDisplayed]=useState([])
+  const[search,setSearch]=useState("");
+  const[query,setQuery]=useState({});
+  const[searchParams,setSearchParams]=useSearchParams();
+
+  useEffect(()=>{
+    setDisplayed(products)
+    setQuery(getInitialQuery(searchParams));
+  },[products]);
+
+  useEffect(()=>{
+    setSearchParams(query)
+    setSearch(query.search || "")
+    let finalProducts=searchProducts(products,query.search)
+    finalProducts=filterProducts(finalProducts,query.category)
+    setDisplayed(finalProducts)
+  },[query])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length&&<p>Loading... </p>}
-        {products.map((p)=>(<Card key={p.id} data={p}/>))}
+    <>
+      <SearchBox search={search} setSearch={setSearch} setQuery={setQuery}/>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!displayed.length&&<Loader />}
+          {displayed.map((p)=>(<Card key={p.id} data={p}/>))}
+        </div>
+        <Sidebar setQuery={setQuery} query={query}/>
       </div>
-      <div>sidebar</div>
-    </div>
+    </>
   )
 }
 
